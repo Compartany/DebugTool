7e640822603a47b1c259c3b2ca52dcac03cd67eb
Compartany <compartany@foxmail.com>
2021-11-14 21:56:18 +0800

    update

--- scripts/mods.lua
+++ scripts/mods.lua
@@ -1,9 +1,11 @@
 dem = mod_loader.mods.EnvManipulators
 drs = mod_loader.mods.RndSquad
+dai = mod_loader.mods.IntelligentAI
 
 local Mods = {
     em = dem,
-    rs = drs
+    rs = drs,
+    ai = dai
 }
 
 function Mods:Load()
